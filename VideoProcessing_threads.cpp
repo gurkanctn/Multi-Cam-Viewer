8ee898c35ce2c88ae603850ca96a61e35133676a
Aircraft Guy <8074474+gurkanctn@users.noreply.github.com>
2022-04-12 11:01:26 +0300

    Added new feature
    corrected previews,
    improved screen layout,
    added new features (#9): mouse selected Input regions are highlighted at Output, GURKAN text is rendered onto the Output
    corrected motion amplifier

--- VideoProcessing_threads.cpp
+++ VideoProcessing_threads.cpp
@@ -3,20 +3,20 @@
 // https://github.com/gurkanctn/Cpp-Projects
 // See bottom of file for Licence
 // Proudly uses the olcPixelGameEngine : https://community.onelonecoder.com/
-// Uses ESCAPI for webcam capturing functions.
+// Uses ESCAPI for webcam capturing functions. 
 
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
 #include "escapi.h"
-
+#include <iostream>
 #include <thread>
 #include <chrono>
 
-int nFrameWidth = 320;  //320
-int nFrameHeight = 240; //240
+int nFrameWidth = 640;  //320
+int nFrameHeight = 480; //240
 
-bool bVideoBufferExists;
+bool bVideoBufferExists = false;
 bool bContinueCapture = true;
 float dTimeBetweenFrames = 0.0f;
 
@@ -62,7 +62,7 @@ struct frame
 	}
 };
 
-frame input, output, prev_input, activity, threshold;
+frame input, output, prev_input, activity;
 
 union RGBint
 	{
@@ -86,7 +86,7 @@ cCapture mCapture[4]; //TODO: make it dynamic at Initialization
 enum ALGORITHM
 {
 	THRESHOLD, MOTION, LOWPASS, CONVOLUTION,
-	SOBEL, MORPHO, MEDIAN, ADAPTIVE,
+	SOBEL, MORPHO, MEDIAN, ADAPTIVE, GURKAN,
 };
 
 enum MORPHOP
@@ -186,8 +186,65 @@ public:
 	}
 
 public:
+
+	std::wstring GCTEXT[6];
+
 	bool OnUserCreate() override
 	{
+		GCTEXT[0].append(L"...XXXX..");
+		GCTEXT[0].append(L"..X.....");
+		GCTEXT[0].append(L"..X.....");
+		GCTEXT[0].append(L"..XXXXX.");
+		GCTEXT[0].append(L"..X...X.");
+		GCTEXT[0].append(L"..X...X.");
+		GCTEXT[0].append(L"..X...X.");
+		GCTEXT[0].append(L"..XXXXX.");
+		
+		GCTEXT[1].append(L"...X.X..");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..X...X.");
+		GCTEXT[1].append(L"..XXXXX.");
+		
+		GCTEXT[2].append(L"..XXXX..");
+		GCTEXT[2].append(L"..X...X.");
+		GCTEXT[2].append(L"..X...X.");
+		GCTEXT[2].append(L"..X.XXX.");
+		GCTEXT[2].append(L"..X...X.");
+		GCTEXT[2].append(L"..X...X.");
+		GCTEXT[2].append(L"..X...X.");
+		GCTEXT[2].append(L"..X...X.");
+		
+		GCTEXT[3].append(L"..X...X.");
+		GCTEXT[3].append(L"..X..XX.");
+		GCTEXT[3].append(L"..XXXX..");
+		GCTEXT[3].append(L"..X.XX..");
+		GCTEXT[3].append(L"..X..XX.");
+		GCTEXT[3].append(L"..X...X.");
+		GCTEXT[3].append(L"..X...X.");
+		GCTEXT[3].append(L"..X...X.");
+		
+		GCTEXT[4].append(L"...XXX..");
+		GCTEXT[4].append(L"..X...x.");
+		GCTEXT[4].append(L"..X...x.");
+		GCTEXT[4].append(L"..XXXXX.");
+		GCTEXT[4].append(L"..X...X.");
+		GCTEXT[4].append(L"..X...X.");
+		GCTEXT[4].append(L"..X...X.");
+		GCTEXT[4].append(L"..X...X.");
+		
+		GCTEXT[5].append(L"..X...X.");
+		GCTEXT[5].append(L"..X...X.");
+		GCTEXT[5].append(L"..XX..X.");
+		GCTEXT[5].append(L"..XXX.X.");
+		GCTEXT[5].append(L"..X.XXX.");
+		GCTEXT[5].append(L"..X.XXX.");
+		GCTEXT[5].append(L"..X..XX.");
+		GCTEXT[5].append(L"..X..XX.");
+		
 		printf("\n ************************************* \n");
 		printf("Image processing trials. G.Çetin.\n");
 		return true;
@@ -195,20 +252,21 @@ public:
 
 	int nMainCounter = 0;
 
-	void DrawFrame(frame &f, int x, int y)
+	void DrawFrame(frame &f, int x, int y, int scale)
 	{
-		for (int i = 0; i < nFrameWidth; i++)
-			for (int j = 0; j < nFrameHeight; j++)
+		
+		for (int i = 0; i < nFrameWidth/scale; i++)
+			for (int j = 0; j < nFrameHeight/scale; j++)
 			{
-				int c = (int)std::min(std::max(0.0f, f.pixels[j*nFrameWidth + i] * 255.0f), 255.0f);
+				int c = (int)std::min(std::max(0.0f, f.pixels[(j*scale)*nFrameWidth + (i*scale)] * 255.0f), 255.0f);
 				Draw(x + i, y + j, olc::Pixel(c, c, c));
 			}
 	}
 
-
 	float fThresholdValue = 0.5f;
 	float fLowPassRC = 0.1f;
 	float fAdaptiveBias = 1.1f;
+	float fGCThreshold = 12.5f;
 
 	float *pConvoKernel = kernel_blur;
 
@@ -239,8 +297,7 @@ public:
 		 0.0f, 0.0f, 0.0f,
 		+1.0f, +2.0f, +1.0f,
 	};
-
-
+	
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		//using namespace std::literals::chrono_literals;
@@ -257,6 +314,8 @@ public:
 			if (GetKey(olc::Key::K6).bReleased) algo = MORPHO;
 			if (GetKey(olc::Key::K7).bReleased) algo = MEDIAN;
 			if (GetKey(olc::Key::K8).bReleased) algo = ADAPTIVE;
+			if (GetKey(olc::Key::K9).bReleased) algo = GURKAN;
+
 			if (GetKey(olc::Key::TAB).bReleased) {
 				nSelectedCam = (nSelectedCam + 1) % nCameras; //bReleased or bPressed?
 				//printf(">Cam # %d\n", nSelectedCam);
@@ -281,10 +340,9 @@ public:
 		switch (algo)
 		{
 		case THRESHOLD:
-
 			// Respond to user input
-			if (GetKey(olc::Key::Z).bHeld) fThresholdValue -= 0.1f * dTimeBetweenFrames;
-			if (GetKey(olc::Key::X).bHeld) fThresholdValue += 0.1f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::Z).bHeld) fThresholdValue -= 2.5f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::X).bHeld) fThresholdValue += 2.5f * dTimeBetweenFrames;
 			if (fThresholdValue > 1.0f) fThresholdValue = 1.0f;
 			if (fThresholdValue < 0.0f) fThresholdValue = 0.0f;
 
@@ -295,19 +353,16 @@ public:
 			break;
 
 		case MOTION:
-
 			// Returns the absolute difference between successive frames per pixel
 			for (int i = 0; i < nFrameWidth; i++)
 				for (int j = 0; j < nFrameHeight; j++)
-					output.set(i, j, fabs(input.get(i, j) - prev_input.get(i, j)));
+					output.set(i, j, 10.0f*fabs(input.get(i, j) - prev_input.get(i, j)));
 			break;
 
-
 		case LOWPASS:
-
 			// Respond to user input
-			if (GetKey(olc::Key::Z).bHeld) fLowPassRC -= 0.1f * dTimeBetweenFrames;
-			if (GetKey(olc::Key::X).bHeld) fLowPassRC += 0.1f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::Z).bHeld) fLowPassRC -= 2.5f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::X).bHeld) fLowPassRC += 2.5f * dTimeBetweenFrames;
 			if (fLowPassRC > 1.0f) fLowPassRC = 1.0f;
 			if (fLowPassRC < 0.0f) fLowPassRC = 0.0f;
 
@@ -375,7 +430,7 @@ public:
 					activity.set(i, j, input.get(i, j) > fThresholdValue ? 1.0f : 0.0f);
 				}
 
-			threshold = activity;
+			//threshold = activity;
 
 			switch (morph)
 			{
@@ -480,30 +535,71 @@ public:
 					output.set(i, j, input.get(i, j) > (fRegionSum * fAdaptiveBias) ? 1.0f : 0.0f);
 				}
 			break;
+
+		case GURKAN:
+			// Respond to user input
+			if (GetKey(olc::Key::Z).bHeld) fGCThreshold -= 10.0f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::X).bHeld) fGCThreshold += 10.0f * dTimeBetweenFrames;
+			if (GetKey(olc::Key::SPACE).bHeld) fGCThreshold = 12.5f;
+			int nMouseX = GetMouseX()- 20; //get mouse position in Frame relative coordinates
+			int nMouseY = GetMouseY() - 180;
+			
+			if (fGCThreshold > 25.0f) fGCThreshold = 25.0f;
+			if (fGCThreshold < 0.0f) fGCThreshold = 0.0f;
+
+			// Handle Overlay
+			for (int i = 0; i < nFrameWidth; i++)
+				for (int j = 0; j < nFrameHeight; j++)
+				{
+					float fRegionSum = 0.0f;
+					for (int n = -2; n < +3; n++)
+						for (int m = -2; m < +3; m++)
+							fRegionSum += input.get(i + n, j + m);
+					output.set(i, j, input.get(i, j) > (fRegionSum > fGCThreshold) ? 0.0f : 0.5f);
+				}
+			for (int k = 0; k < 6; k++) //all string
+				for (int i = 0; i < sqrt(GCTEXT[k].size()); i++) //draw each (point of each) character
+					for (int j = 0; j < sqrt(GCTEXT[k].size()); j++)
+					{
+						wchar_t FontPixel = GCTEXT[k][i + j * 8];
+						if (FontPixel == L'X') {
+							output.set(k * 20 + i*2, j*2, 1.0f);
+							output.set(k * 20 + i*2, j*2+1, 1.0f);
+							output.set(k * 20 + i*2+1, j*2, 1.0f);
+							output.set(k * 20 + i*2+1, j*2+1, 1.0f);
+						}
+					}
+			
+			for (int i = 0; i < 10; i++)
+				output.set(nMouseX - 5 + std::rand() %10, nMouseY - 5 + std::rand() %10, 1.0f);
+
+			break;
 		}
+		prev_input = input;
 
 		// DRAW STUFF ONLY HERE
 		Clear(olc::DARK_BLUE);
 		// draw previews of all cameras on the top line
-		DrawFrame(algo == MORPHO ? threshold : mCapture[0].inputFrame, 10, 10);
-		DrawFrame(algo == MORPHO ? threshold : mCapture[1].inputFrame, 90, 10);
-		DrawFrame(algo == MORPHO ? threshold : mCapture[2].inputFrame, 170, 10);
-		DrawFrame(algo == MORPHO ? threshold : mCapture[3].inputFrame, 250, 10);
+		for (int i = 0; i < nCameras; i++) {
+			DrawFrame(mCapture[i].inputFrame, 10+i*80, 10, 4);
+			DrawRect(10 + i * 80, 10, 160, 160, olc::GREEN);
+		}
 		
-		DrawFrame(input, 10, 70);
-		DrawFrame(output, nFrameWidth + 20, 70);
+		DrawFrame(input, 10, 180, 1);
+		DrawFrame(output, nFrameWidth + 20, 180, 1);
 
-		DrawString(150, 255, "INPUT");
-		DrawString(480, 255, "OUTPUT");
+		DrawString(10, 180, "INPUT");
+		DrawString(nFrameWidth + 20, 180, "OUTPUT");
 
-		DrawString(10, 275, "1) Threshold");
-		DrawString(10, 285, "2) Absolute Motion");
-		DrawString(10, 295, "3) Low-Pass Temporal Filtering");
-		DrawString(10, 305, "4) Convolution (Blurring/Sharpening)");
-		DrawString(10, 315, "5) Sobel Edge Detection");
-		DrawString(10, 325, "6) Binary Morphological Operations (Erosion/Dilation)");
-		DrawString(10, 335, "7) Median Filter");
-		DrawString(10, 345, "8) Adaptive Threshold");
+		DrawString(10, nFrameHeight + 210, "1) Threshold");
+		DrawString(10, nFrameHeight + 220, "2) Absolute Motion");
+		DrawString(10, nFrameHeight + 230, "3) Low-Pass Temporal Filtering");
+		DrawString(10, nFrameHeight + 240, "4) Convolution (Blurring/Sharpening)");
+		DrawString(10, nFrameHeight + 250, "5) Sobel Edge Detection");
+		DrawString(10, nFrameHeight + 260, "6) Binary Morphological Operations (Erosion/Dilation)");
+		DrawString(10, nFrameHeight + 270, "7) Median Filter");
+		DrawString(10, nFrameHeight + 280, "8) Adaptive Threshold");
+		DrawString(10, nFrameHeight + 290, "9) GURKAN");
 
 		bVideoBufferExists = false;
 
@@ -531,13 +627,16 @@ public:
 			if (morph == EDGE) DrawString(10, 385, "Current operation = EDGE");
 			DrawString(10, 395, "Change Iterations with A and S keys");
 			DrawString(10, 405, "Current iteration count = " + std::to_string(nMorphCount));
-
-
 			break;
 
 		case ADAPTIVE:
 			DrawString(10, 375, "Change adaptive threshold bias with Z and X keys");
 			DrawString(10, 385, "Current value = " + std::to_string(fAdaptiveBias));
+			break;
+
+		case GURKAN:
+			DrawString(10, 375, "Change adaptive GURKAN threshold with Z and X keys");
+			DrawString(10, 385, "Current value = " + std::to_string(fGCThreshold));
 			break;
 
 		default:
@@ -554,7 +653,7 @@ int main()
 	WIP_ImageProcessing demo;
 	
 
-	if (demo.Construct(670, 460, 1, 1)) {
+	if (demo.Construct(1400, 800, 1, 1)) {
 		initCameraForCapture();
 		std::thread thrCapture(CaptureVideo);
 		demo.Start();
